b8b2554b4c63743007986fe4700de675ce4f387d
seif mortada <seifmetwaly78@gmail.com>
2025-03-03 10:45:36 +0200

    -Create `CoreModule` for dependency injection of Room database, DAO, and `ExperienceRepository`. -Create `NetworkModule` for dependency injection of Retrofit and `ExperienceService`. -Add `retrofit2-kotlinx-serialization-converter` to handle JSON serialization/deserialization. -Update `libs.versions.toml` with new libraries and versions.

---
+++ app/src/main/java/com/seifmortada/applications/aroundegypt/core/di/NetworkModule.kt
@@ -0,0 +1,42 @@
+package com.seifmortada.applications.aroundegypt.core.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.seifmortada.applications.aroundegypt.core.data.network.api.ApiConstant.BASE_URL
+import com.seifmortada.applications.aroundegypt.core.data.network.api.ExperienceService
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+
+val networkModule = module {
+
+    single {
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor(get<HttpLoggingInterceptor>())
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    single {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(get())
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+    single {
+        get<Retrofit>().create(ExperienceService::class.java)
+    }
+
+}
